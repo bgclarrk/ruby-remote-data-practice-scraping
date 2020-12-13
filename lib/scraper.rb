5b4808f4954864925ecd226e33bd5a61bebfdb29
+ <+>
2020-12-13 15:03:39 -0700

    commit tree 6be20a8a7474fd3f4509d1668880d6afb129acc5

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,16 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+courses_data = doc.css(".text-4GLMvr")
+
+courses = []
+i = 0
+while i < 4
+    courses << courses_data.css(".title-oE5vT4")[i].text
+    i += 1
+end
+
+courses
